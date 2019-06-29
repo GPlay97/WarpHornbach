6307815eb8b86e5ea8f6cf21e5d7aa89cc8b7a7f
GPlay97 <GPlay97@users.noreply.github.com>
2019-06-29 21:24:41 +0200

    update db template for activity and user

--- server/template.sql
+++ server/template.sql
@@ -1,5 +1,11 @@
-CREATE TABLE user (
+CREATE TABLE IF NOT EXISTS user (
     username VARCHAR(222) PRIMARY KEY,
-    pwd_hash VARCHAR(222),
+    pwd_hash VARCHAR(222) NOT NULL,
     administrator TINYINT(1) DEFAULT 0
+);
+
+CREATE TABLE IF NOT EXISTS activity (
+    id INT(11) PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(222) NOT NULL,
+    activity_time INT(11) NOT NULL
 );
