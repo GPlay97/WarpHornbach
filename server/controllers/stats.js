6b33ff1fca95028bfa7ee867b83de1697afdbc5f
GPlay97 <GPlay97@users.noreply.github.com>
2019-07-12 21:45:26 +0200

    getStats for last 7 days

--- server/controllers/stats.js
+++ server/controllers/stats.js
@@ -1,7 +1,11 @@
 const db = require('../utils/db');
 
 const getStats = async (req, res, next) => {
-    db.query('SELECT COUNT(*) as personal, (SELECT COUNT(*) FROM activity) as total FROM activity WHERE username = ?', [req.session.username])
+    const sevenDaysAgo = parseInt(new Date() / 1000 - 86400 * 7);
+
+    db.query('SELECT COUNT(*) as personal, (SELECT COUNT(*) FROM activity WHERE activity_time >= ?) as total FROM activity WHERE username = ? AND activity_time >= ?', [
+        sevenDaysAgo, req.session.username, sevenDaysAgo
+    ])
         .then((result) => res.json(result[0]))
         .catch((err) => next(err));
 };
